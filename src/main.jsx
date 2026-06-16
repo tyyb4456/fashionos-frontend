e1794fa3060590c3059e5ef41b853f96be1c3d43
Tayyab <igntayyab@gmail.com>
2026-06-16 19:20:36 +0500

    little

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from './context/ThemeContext'
 import App from './App'
 import './index.css'
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY || "pk_test_bmVhdC1wZW5ndWluLTQ5LmNsZXJrLmFjY291bnRzLmRldiQ"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
